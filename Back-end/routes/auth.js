32952a88dbd8dffde92a8e24cac8a76e720b7f14
Naruy666 <marcossanteiro92@gmail.com>
2025-10-14 13:04:15 -0300

    Implementando lógica de login básica funcional com leitura de JSON de data.json/

--- Back-end/routes/auth.js
+++ Back-end/routes/auth.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-// Importa a função login do controller
+
 const { login } = require('../controllers/authController');
 
-// Rota de teste opcional
+
 router.get('/', (req, res) => {
   res.json({ message: 'rota auth funcionando!' });
 });
 
-// Rota POST /auth/login usando o controller real
+
 router.post('/login', login);
 
-// Exporta o router
+
 module.exports = router;
